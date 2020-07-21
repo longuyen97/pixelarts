1e7df9c435dda8ff0bd5813d9d29609ea6cb1cd6
Long Nguyen <long.nguyen2306@protonmail.com>
2020-07-21 17:43:04 +0200

    save

--- src/main/java/de/longuyen/core/impl/SobelConvolution.java
+++ src/main/java/de/longuyen/core/impl/SobelConvolution.java
@@ -2,12 +2,20 @@ package de.longuyen.core.impl;
 
 import de.longuyen.core.Convolution;
 
-import java.awt.image.BufferedImage;
-
 public class SobelConvolution implements Convolution {
+    private static final int [][] KERNEL = new int[][]{
+        new int[]{1, 2, 3},
+        new int[]{1, 2, 3},
+        new int[]{1, 2, 3}
+    };
 
     @Override
-    public BufferedImage convert(final BufferedImage bufferedImage) {
-        return null;
+    public int[][] getKernel() {
+        return KERNEL;
+    }
+
+    @Override
+    public int getSlide() {
+        return 1;
     }
 }
